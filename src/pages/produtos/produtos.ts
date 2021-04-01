76e16359182573aa115980d5d10c8adf07720d67
Maycon Carvalho <maycon.arth.carvalho@gmail.com>
2021-04-01 11:24:37 -0300

    Carregando produtos de uma dada categoria backend

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ProdutoService } from '../../app/services/domain/produto.service';
 import { ProdutoDTO } from '../../models/produto.dto';
 
 @IonicPage()
@@ -11,21 +12,19 @@ export class ProdutosPage {
 
   items : ProdutoDTO[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public produtoService: ProdutoService
+  ) {
   }
 
   ionViewDidLoad() {
-    this.items = [
-      {
-        id: "1",
-        nome: 'Mouse',
-        preco: 80.99
+    let categoria_id = this.navParams.get('categoria_id');
+    this.produtoService.findByCategoria(categoria_id)
+      .subscribe(response => {
+        this.items = response['content'];
       },
-      {
-        id: "2",
-        nome: 'Teclado',
-        preco: 100.00
-      }
-    ]
+      error => {});
   };
 }
